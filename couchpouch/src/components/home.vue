416acb531fae6099395eca6c6c2f6302366d2d4f
rahul <ymalhotra41@gmail.com>
2020-02-08 00:01:50 +0530

    setup complete

---
+++ couchpouch/src/components/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    this is testing. All set. Ready to Go!!!
+  </div>  
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
